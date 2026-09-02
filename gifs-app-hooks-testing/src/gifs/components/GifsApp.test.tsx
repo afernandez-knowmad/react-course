e7a771fab345e5e59e6fbc9e2f7469700ee73eb7
Afernandez <afernandez.herrera@knowmadmood.com>
2026-09-02 15:51:41 +0200

    chore: remove unused import from GifsApp test file

--- gifs-app-hooks-testing/src/gifs/components/GifsApp.test.tsx
+++ gifs-app-hooks-testing/src/gifs/components/GifsApp.test.tsx
@@ -2,7 +2,7 @@
 
 
 import { describe, expect, test } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import { GifsApp } from './GifsApp';
 
 describe('GifsApp', () => {
