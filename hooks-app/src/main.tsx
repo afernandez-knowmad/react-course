ddd03fc353e9b006006f191d8e87fa4ac86d5c69
Afernandez <afernandez.herrera@knowmadmood.com>
2026-09-03 18:10:37 +0200

    feat: add shadcn/ui components (button, card, checkbox, input)

--- hooks-app/src/main.tsx
+++ hooks-app/src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { FoscusScreen } from './useRef/FoscusScreen'
+import { TasksApp } from './useReducer/TasksApp.tsx'
+// import { FoscusScreen } from './useRef/FoscusScreen'
 // import { HooksApp } from './HooksApp.tsx'
 // import { TrafficLight } from './useState/TrafficLight.tsx'
 // import { TrafficLightWithEffect } from './useEffect/TrafficLightWithEffect.tsx'
@@ -11,6 +12,6 @@ import { FoscusScreen } from './useRef/FoscusScreen'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <TrafficLight /> */}
-    <FoscusScreen />
+    <TasksApp />
   </StrictMode>,
 )
