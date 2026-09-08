6e67e0031488e15c0720b4148c22b892607ad0a6
Afernandez <afernandez.herrera@knowmadmood.com>
2026-09-08 15:55:30 +0200

    feat: add UI components for breadcrumb, button, card, input, navigation menu, progress, tabs, and hero grid

--- heroes-app/src/main.tsx
+++ heroes-app/src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { Button } from './components/ui/button'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { HeroesApp } from './HeroesApp';
+
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <h1>Hola mundo</h1>
-    <Button>Button yeah</Button>
-  </StrictMode>,
-)
+    <HeroesApp />
+  </StrictMode>
+);
