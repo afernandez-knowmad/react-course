16a73e1a30c4d322fa82d94244e91ff4e3c1b05a
Afernandez <afernandez.herrera@knowmadmood.com>
2026-09-10 13:29:10 +0200

    feat: sync strength filter and accordion state to URL parameters

--- heroes-app/src/heroes/pages/search/SearchPage.tsx
+++ heroes-app/src/heroes/pages/search/SearchPage.tsx
@@ -11,11 +11,12 @@ export const SearchPage = () => {
 
   const [searchParams] = useSearchParams();
 
-  const name = searchParams.get('name') ?? '';
+  const name = searchParams.get('name') ?? undefined;
+  const strength = searchParams.get('strength') ?? undefined;
 
   const { data: searchData = [] } = useQuery({
-    queryKey: ['search', { name }], // Los argumentos de la funcion deberian ser siempre queryKeys
-    queryFn: () => searchHeroesAction({ name }),
+    queryKey: ['search', { name, strength }], // Los argumentos de la funcion deberian ser siempre queryKeys
+    queryFn: () => searchHeroesAction({ name, strength }),
     staleTime: 1000 * 60 * 5 // 5 minutos
   });
 
