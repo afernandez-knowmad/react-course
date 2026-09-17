d4cd4d702976cd988ac37f549cbe18171377ac72
Afernandez <afernandez.herrera@knowmadmood.com>
2026-09-17 11:29:57 +0200

    feat: set up TesloShopApp with router and react query providers

--- teslo-shop/src/TesloShopApp.tsx
+++ teslo-shop/src/TesloShopApp.tsx
@@ -1,5 +1,16 @@
+import { RouterProvider } from "react-router"
+import { tesloMainRouter } from "./router/teslo-main.router"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+
+const queryClient = new QueryClient();
+
 export const TesloShopApp = () => {
   return (
-    <div>TesloShopApp</div>
+    <QueryClientProvider client={queryClient}>
+      {/* The rest of your application */}
+      <RouterProvider router={tesloMainRouter} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
--- teslo-shop/src/main.tsx
+++ teslo-shop/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { RouterProvider } from 'react-router'
 import './index.css'
-import { tesloMainRouter } from './router/teslo-main.router'
+import { TesloShopApp } from './TesloShopApp'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={tesloMainRouter} />
+    <TesloShopApp />
   </StrictMode>,
 )
